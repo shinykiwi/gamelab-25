140add9f38a21a5f1381b2bc2470578609a7cf4b
Kira <kafountain5@gmail.com>
2025-01-09 10:34:03 -0500

    moved stuff

--- Assets/Code/Scripts/UIManager.cs
+++ Assets/Code/Scripts/UIManager.cs
@@ -5,7 +5,8 @@ public class UIManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GameObject mainMenu;
+        GameObject pauseMenu;
     }
 
     // Update is called once per frame
