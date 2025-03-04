98fd647485c45873939e16881ecfdb050bd54292
Kira <kafountain5@gmail.com>
2025-03-04 10:32:51 -0500

    try this

--- Assets/Level/Prefabs/Tiles/PrefabBrush.cs
+++ Assets/Level/Prefabs/Tiles/PrefabBrush.cs
@@ -1,9 +1,10 @@
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
+#if UNITY_EDITOR
 [CreateAssetMenu(fileName = "New Brush", menuName = "Brushes/GameObject Brush")]
 [CustomGridBrush(false, true, false, "Prefab Brush")]
-
+#endif
 public class PrefabBrush : GameObjectBrush
 {
 
