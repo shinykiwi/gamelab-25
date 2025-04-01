d637de52c623316a7c77eaff4e184736b88b5b18
Samuel <samuel.laporte@polymtl.ca>
2025-04-01 08:47:15 -0400

    Fix build

--- Assets/Code/Scripts/Enemy.cs
+++ Assets/Code/Scripts/Enemy.cs
@@ -2,7 +2,6 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
